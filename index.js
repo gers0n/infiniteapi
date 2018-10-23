9a7aeeaf8abf41b53b98658093e0fe497f9525fc
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2018-10-23 13:21:59 -0400

    add basic mongoose, graphql and node server for api

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import mongoose from "mongoose"
+import graphlHTTP from 'express-graphql'
+import schema from './schema'
+
+
+const app = express();
+const PORT = 8080;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/gql_db');
+
+app.get("/api", (req, res) => {
+  res.json({
+    msg: 'Welc0me to GraphQL'
+  })
+});
+
+app.use("/graphql", graphlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
---
+++ schema.js
@@ -0,0 +1,24 @@
+import {
+    makeExecutableSchema
+} from 'graphql-tools';
+import {
+    resolvers
+} from './resolvers';
+const typeDefs = `
+
+type Product {
+  _id: ID!
+  title: String!
+  qty: Int
+ }
+type Query {
+  allProducts: [Product]
+ }
+`;
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+export default schema;
