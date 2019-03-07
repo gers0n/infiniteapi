48c24780e7d95cfd080f80e2ee0ecdd4035c0325
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2019-03-07 03:12:30 -0400

    Add search by Index Values for all Movies

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,7 +1,6 @@
 import Actor from "./models/Actor";
 import Movie from "./models/Movie";
-import {Schema} from 'mongoose'
-
+import { Schema } from "mongoose";
 
 const Resolver = docs => {
   docs.id = docs._id.toString();
@@ -10,74 +9,93 @@ const Resolver = docs => {
 const allActors = async () => {
   return (await Actor.find()).map(Resolver);
 };
-const getActor = async (_id) => {
+const getActor = async _id => {
   return await Actor.findOne({ id: Schema.Types.ObjectId(_id) });
+};
+const createMovieTextIndex = () => {
+  Movie.createIndex(
+    {
+      title: "text",
+      year: "text",
+      synopsis: "text"
+    },
+    {
+      weights: {
+        title: 10,
+        year: 3,
+        synopsis: 5
+      },
+      name: "TextIndex"
+    }
+  );
 };
 const emptyFunc = x => x;
 const filterDatesMapper = f => {
-  if(f.released) return  {
-    ...f,
-    released:  {
-      $gte: new Date(f.released.from),
-      $lte: new Date(f.released.to)
-    } 
-  };
-  if( f.dateUpdated ) return  {
-    ...f,
-    dateUpdated:  {
-      $gte: new Date(f.dateUpdated.from),
-      $lte: new Date(f.dateUpdated .to)
-    }
-  }
-  return f
+  let mapped = {};
+
+  if (f.released)
+    mapped = {
+      ...f,
+      released: {
+        $gte: new Date(f.released.from),
+        $lte: new Date(f.released.to)
+      }
+    };
+  if (f.dateUpdated)
+    mapped = {
+      ...f,
+      dateUpdated: {
+        $gte: new Date(f.dateUpdated.from),
+        $lte: new Date(f.dateUpdated.to)
+      }
+    };
+    if (f.search)
+    mapped = {
+      ...f,
+      $text: { $search: f.search },
+      search: null
+    };
+  return mapped;
 };
 
 export const resolvers = {
   Query: {
     // allActors,
     // getActor,
-    async allMovies(parent, {filter, orderBy, limit, skip}) {
+    async allMovies(parent, { filter, orderBy, limit, skip }) {
       // console.log("got request", JSON.stringify( arguments));
       const dateMapper = movie => {
         // movie.released = "{1}/{2}/{3}".format(movie.released.getDay(), movie.released.getMonth(), movie.released.getYear);
         return movie;
       };
-      
-      if(filter) {
-        return (
-          await Movie
-            .find(filterDatesMapper(filter))
-            .sort(orderBy)
-            .skip(skip)
-            .limit(limit)
-          )
-          .map(Resolver)
-          .map(dateMapper)
-      } else {
-        return (
-          await Movie.find()
+
+      if (filter) {
+        return (await Movie.find(filterDatesMapper(filter))
           .sort(orderBy)
           .skip(skip)
-          .limit(limit)
-          )
-          .map(Resolver).map(dateMapper)
+          .limit(limit))
+          .map(Resolver)
+          .map(dateMapper);
+      } else {
+        return (await Movie.find()
+          .sort(orderBy)
+          .skip(skip)
+          .limit(limit))
+          .map(Resolver)
+          .map(dateMapper);
       }
     },
-    async getLastMovies(){
+    async getLastMovies() {
       var date = new Date();
       let filter = {
         released: {
           $gte: date.getFullYear().toString(),
-          $lt: (date.getFullYear()+1).toString()
+          $lte: (date.getFullYear() + 1).toString()
         }
       };
-      return (
-        await Movie
-          .find(filter)
-          .sort({released: -1})
-          .limit(10)
-        )
-        .map(Resolver)
+      return (await Movie.find(filter)
+        .sort({ released: -1 })
+        .limit(10)).map(Resolver);
     }
     // async allGenres() {
     //   return (await Genre.find()).map(Resolver);
